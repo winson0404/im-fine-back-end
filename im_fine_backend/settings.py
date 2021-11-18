bb4d032306c0ddce8250d943517cce993e12e3ec
winson <weixiong8489@gmail.com>
2021-11-18 12:59:30 +0800

    fixed deployment error

--- im_fine_backend/settings.py
+++ im_fine_backend/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # additionals,
-    'django_seed',
     'rest_framework_swagger',
     'drf_yasg',
     'rest_framework',
